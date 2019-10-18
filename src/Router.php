588a735ce385d781c3573d82454834bc17271ea7
Brent Scheffler <brent@brentscheffler.com>
2019-10-18 16:31:48 -0700

    Adding convenience methods to Router class, addRoute() and setRoutes().

--- src/Router.php
+++ src/Router.php
@@ -30,24 +30,46 @@ class Router
     {
         $this->resolver = $resolver;
         $this->routes = $routes;
-    }
+	}
+
+	/**
+	 * Set the routes.
+	 *
+	 * @param array<string, mixed> $routes
+	 * @return void
+	 */
+	public function setRoutes(array $routes): void
+	{
+		$this->routes = $routes;
+	}
+
+	/**
+	 * Add a route handler.
+	 *
+	 * @param string $key
+	 * @param string|array|callable $handler
+	 * @return void
+	 */
+	public function addRoute(string $key, $handler): void
+	{
+		$this->routes[$key] = $handler;
+	}
 
     /**
      * Match the Message to a route handler.
      *
      * Returns a handler on successful route match.
+	 *
+	 * Return null if route could not be resolved.
      *
      * @param Message $message
-     * @return mixed|null
+     * @return string|array|callable|null
      */
     public function resolve(Message $message)
     {
-        foreach( $this->routes as $route => $handler ){
-
-            if( \call_user_func_array($this->resolver, [$message, $route]) ){
-
+        foreach( $this->routes as $key => $handler ){
+            if( \call_user_func_array($this->resolver, [$message, $key]) ){
                 return $handler;
-
             }
         }
 
